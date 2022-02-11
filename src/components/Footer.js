d6c99fe976b2fb86436e5ff8c739ba9b73c1b855
Motolani-D <motolanid@gmail.com>
2022-02-11 18:30:50 +0100

    positioned the elipse and added footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,33 @@
 import React from "react";
-
-import React from "react";
+import "./footer.css";
 
 const Footer = () => {
-  return <div className="div-footer"></div>;
+  return (
+    <div className="div-footer">
+      <div className="elipse">
+        <h5>Hello</h5>
+      </div>
+      <div className="footer-body">
+        <div float="left">
+          <img src="" alt="background" />
+          <h5>foliofix is a product of 2DRUMAB</h5>
+          <h5>
+            <span>&copy; 2022</span>
+          </h5>
+          <p>www.gmccohort6.com.ng</p>
+        </div>
+        <div float="left">
+          <h3>Contact</h3>
+          <p>+234-812-345-4566</p>
+          <p>Email: velox@gmail.com</p>
+        </div>
+        <div float="left">
+          <h3>Product</h3>
+          <p>Pages</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
